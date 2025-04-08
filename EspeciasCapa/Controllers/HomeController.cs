b2b88a76955217080c210ddd1706c77e9b373347
rodrv09 <137659985+rodrv09@users.noreply.github.com>
2025-04-07 19:47:22 -0600

    _Layout

--- EspeciasCapa/Controllers/HomeController.cs
+++ EspeciasCapa/Controllers/HomeController.cs
@@ -17,6 +17,10 @@ namespace EspeciasCapa.Controllers
         {
             return View();
         }
+        public IActionResult Inventario()
+        {
+            return View();
+        }
 
         public IActionResult Privacy()
         {
